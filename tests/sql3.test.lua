30929c343ba10ac7375ec646deecdb453e9ec210
carywill2 <carywill.k1@gmail.com>
2024-01-07 17:07:00 +0800

    updated

--- tests/sql3.test.lua
+++ tests/sql3.test.lua
@@ -8,7 +8,7 @@ local roam_folder = "~/OrgRoam"
 local utils = require("org-roam.utils")
 
 describe("sql", function()
-	it("open", function()
+	it("build database for id links of files", function()
 		local db = sqlite:open(dbpath)
 		utils.process_folder(roam_folder, function(matches)
 			if not db:exists(table_name) then
@@ -51,18 +51,27 @@ describe("sql", function()
 			end
 		end)
 		db:close()
-		-- vim.print(vim.inspect(records))
-		-- assert.equals(records, true)
-		-- db:insert(table_name, { id = 1234556, filepath = "1234", fileid = "1234", idlinks = "adf2" })
 	end)
 	it("with open", function()
-		-- FIXME: Still couldn'tget it to work
-		--
-		-- local records = sqlite:with_open(dbpath, function(db)
-		-- return sqlite:select(table_name, { where = { fileid = "123" } })
 		-- TODO: db:close should be put at the end
 		-- I dont know why I cannot get with_open to work
-		-- end)
-		-- assert.equals(records, true)
+	end)
+
+	it("get backlinks", function()
+		local current_file_id = "C6DDDD61-9A5B-4BD6-BD65-C521B71D5D35"
+		local current_file_path = "/Users/cary/OrgRoam/20240104201813_test2.org"
+		local db = sqlite:open(dbpath)
+		local back_links = {}
+		local records = db:select(table_name)
+		for index, value in ipairs(records) do
+			local id_links = vim.json.decode(value.id_links)
+			for _, id_link in ipairs(id_links) do
+				if id_link.id == current_file_id then
+					table.insert(back_links, id_link)
+				end
+			end
+		end
+		print(vim.inspect(back_links))
+		db:close()
 	end)
 end)
